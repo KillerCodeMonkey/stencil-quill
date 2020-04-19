939785789aa2fabe9896b213b27425edf032a50a
Bengt Weiße <bengtler@gmail.com>
2020-04-19 09:13:17 +0200

    test: quill-view-html

---
+++ src/components/quill-view-html/quill-view-html.spec.tsx
@@ -0,0 +1,70 @@
+import { QuillViewHTMLComponent } from './quill-view-html';
+import { newSpecPage, SpecPage } from '@stencil/core/testing';
+
+
+describe('QuillViewHTMLComponent', () => {
+  let page: SpecPage;
+
+  beforeEach(async () => {
+    page = await newSpecPage({
+      components: [QuillViewHTMLComponent]
+    });
+  });
+
+  it('renders and sets default snow theme class', async () => {
+    await page.setContent('<quill-view-html></quill-view-html>')
+
+    expect(page.root).toEqualHtml(`
+      <quill-view-html>
+        <div class="ql-container ql-snow quill-view-html">
+          <div class="ql-editor"></div>
+        </div>
+      </quill-view-html>
+    `);
+
+    expect(page.rootInstance.theme).toEqual('snow');
+    expect(page.rootInstance.content).toBe(undefined);
+  });
+
+  it('renders initial content', async () => {
+    await page.setContent('<quill-view-html content="<p>Hallo</p>"></quill-view-html>')
+
+    expect(page.root).toEqualHtml(`
+      <quill-view-html content="<p>Hallo</p>">
+        <div class="ql-container ql-snow quill-view-html">
+          <div class="ql-editor">
+            <p>
+              Hallo
+            </p>
+          </div>
+        </div>
+      </quill-view-html>
+    `);
+
+    expect(page.rootInstance.content).toEqual('<p>Hallo</p>');
+  });
+
+  it('renders content update', async () => {
+    await page.setContent('<quill-view-html content="<p>Hallo</p>"></quill-view-html>')
+
+    const quillView = page.body.querySelector('quill-view-html')
+    quillView.content = '<p>test</p>'
+
+    await page.waitForChanges()
+
+    expect(page.root).toEqualHtml(`
+      <quill-view-html content="<p>Hallo</p>">
+        <div class="ql-container ql-snow quill-view-html">
+          <div class="ql-editor">
+            <p>
+              test
+            </p>
+          </div>
+        </div>
+      </quill-view-html>
+    `);
+
+
+    expect(page.rootInstance.content).toBe('<p>test</p>');
+  });
+});
